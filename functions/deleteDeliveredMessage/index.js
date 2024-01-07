7810c0a80cd9c80b3e544a78fa8c814c75bc024e
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2024-01-07 20:38:13 +0530

    feat: delete message function

---
+++ functions/deleteDeliveredMessage/index.js
@@ -0,0 +1,53 @@
+import { HttpsError } from "firebase-functions/v2/https";
+
+const func = async (db, request) => {
+  const { groupId, messageId } = request.data;
+
+  const groupSnap = await db.collection("groups").doc(groupId).get();
+  let group;
+  if (!groupSnap.exists()) {
+    throw new HttpsError("not-found", "Group not found");
+  }
+  group = groupSnap.data();
+
+  if (!group.members.includes(request.auth.uid)) {
+    throw new HttpsError("permission-denied", "Not a member of the group");
+  }
+
+  const msgSnap = await db
+    .collection("groups")
+    .doc(groupId)
+    .collection("messages")
+    .doc(messageId)
+    .get();
+  let msg;
+  if (!msgSnap.exists()) {
+    throw new HttpsError("not-found", "Message not found");
+  }
+  msg = msgSnap.data();
+
+  let numMembers = group.members.length;
+  group.members.forEach((memberId) => {
+    let seenTimestamp = group.seen[memberId];
+    if (seenTimestamp) {
+      seenTimestamp = seenTimestamp.toDate();
+    }
+    if (seenTimestamp >= msg.timestamp) numMembers -= 1;
+  });
+
+  if (numMembers <= 0) {
+    await db
+      .collection("groups")
+      .doc(groupId)
+      .collection("messages")
+      .doc(messageId)
+      .delete();
+  } else {
+    throw new HttpsError(
+      "failed-precondition",
+      "Message not yet delivered to every member"
+    );
+  }
+};
+
+export default func;
---
+++ functions/deleteExpiredAnonymousUsers/index.spec.js
@@ -0,0 +1,118 @@
+import { jest, beforeAll, afterAll, describe, expect } from "@jest/globals";
+import test from "firebase-functions-test";
+
+const { cleanup, wrap } = test();
+
+const mockListUsersStub = jest.fn();
+const mockDeleteUsersStub = jest.fn();
+const today = new Date();
+const nonExpiredDate = today.toISOString().slice(0, 10);
+
+let expiredDate = new Date();
+expiredDate.setDate(today.getDate() - 31);
+expiredDate = expiredDate.toISOString().slice(0, 10);
+
+mockListUsersStub
+  .mockReturnValueOnce({
+    users: [
+      { uid: "1", providerData: [], metadata: { creationTime: expiredDate } },
+      {
+        uid: "2",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      {
+        uid: "3",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      { uid: "4", providerData: [], metadata: { creationTime: expiredDate } },
+      {
+        uid: "5",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      { uid: "6", providerData: [], metadata: { creationTime: expiredDate } },
+      {
+        uid: "7",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      { uid: "8", providerData: [], metadata: { creationTime: expiredDate } },
+    ],
+    pageToken: 1,
+  })
+  .mockReturnValueOnce({
+    users: [
+      { uid: "9", providerData: [], metadata: { creationTime: expiredDate } },
+      {
+        uid: "10",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      {
+        uid: "11",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      {
+        uid: "12",
+        providerData: [],
+        metadata: { creationTime: expiredDate },
+      },
+      {
+        uid: "13",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      {
+        uid: "14",
+        providerData: [],
+        metadata: { creationTime: expiredDate },
+      },
+      {
+        uid: "15",
+        providerData: [],
+        metadata: { creationTime: nonExpiredDate },
+      },
+      {
+        uid: "16",
+        providerData: [],
+        metadata: { creationTime: expiredDate },
+      },
+    ],
+    pageToken: null,
+  });
+
+jest.mock("firebase-admin", () => ({
+  initializeApp: jest.fn(),
+  auth: jest.fn(() => ({
+    listUsers: mockListUsersStub,
+    deleteUsers: mockDeleteUsersStub,
+  })),
+  firestore: jest.fn(),
+}));
+
+describe("openchat-functions", () => {
+  let functions;
+  beforeAll(async () => {
+    functions = await import("../index");
+  });
+
+  afterAll(() => {
+    cleanup();
+  });
+
+  describe("accountcleanup", () => {
+    it("should delete all anonymous users older than 30 days", async () => {
+      const wrapped = wrap(functions.accountcleanup);
+      await wrapped();
+
+      console.log(mockDeleteUsersStub.mock.calls);
+      expect(mockDeleteUsersStub.mock.calls).toEqual([
+        [["1", "4", "6", "8"]],
+        [["9", "12", "14", "16"]],
+      ]);
+    });
+  });
+});
